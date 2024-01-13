85948dbde24a93aa40de3b569a738daad64fcde2
Damilola Ogunsakin <ogunsakin191@gmail.com>
2024-01-13 19:24:33 +0000

    Initial migration

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookingController extends Controller
+{
+    public function availability(){
+
+    }
+
+    public function availabilityPricing(){
+
+    }
+
+    public function createBooking(){
+
+    }
+
+    public function cancelBooking(){
+
+    }
+
+    public function updateBooking(){
+
+    }
+}
